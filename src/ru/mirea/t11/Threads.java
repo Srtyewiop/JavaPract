5472047dc47d34b62fecf96cff876d3bde734e03
Zyjc <@wsa.as>
2020-10-12 15:17:34 +0300

    task 11 complete(and it's so)

--- src/ru/mirea/t11/Threads.java
+++ src/ru/mirea/t11/Threads.java
@@ -5,6 +5,7 @@ import java.util.concurrent.locks.ReentrantLock;
 
 public class Threads {
     static int totalSum;
+    static int totalFalseSum;
 
     public static void main(String[] args) throws InterruptedException {
         long startTime = System.currentTimeMillis();
@@ -21,14 +22,26 @@ public class Threads {
         }
         long endTime = System.currentTimeMillis();
         System.out.println("total time: " + (endTime - startTime));
+        System.out.println("total false sum: " + totalFalseSum);
         System.out.println("total sum: " + totalSum);
     }
 
     private static void work(int i) {
         long startTime = System.currentTimeMillis();
-        long result = doHardWork(i * 5, (int)Math.pow(12, 5));
+        long falseRes = DoStupidWork(i * 5, (int) Math.pow(12, 5));
+        long result = doHardWork(i * 5, (int) Math.pow(12, 5));
         long endTime = System.currentTimeMillis();
+
         System.out.println(i + ": " + result + " | " + (endTime - startTime));
+        System.out.println(i + "(false): " + falseRes + " | " + (endTime - startTime));
+    }
+    private static long DoStupidWork(int start, int count) {
+        long a = 0;
+        for (int i = 0; i < count; i++) {
+            a += (start + i) * (start - i) * 0.3;
+            totalFalseSum++;
+        }
+        return a;
     }
 
     private synchronized static long doHardWork(int start, int count) {
@@ -39,4 +52,6 @@ public class Threads {
         }
         return a;
     }
+
 }
+
