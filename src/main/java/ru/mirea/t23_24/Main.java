0eb7ecff7bffdf08a5eba031052cb3d333499218
srtyewiop@gmail.com <srtyewiop@gmail.com>
2020-11-25 18:28:18 +0300

    task 23_24

--- src/main/java/ru/mirea/t23_24/Main.java
+++ src/main/java/ru/mirea/t23_24/Main.java
@@ -2,17 +2,17 @@ package ru.mirea.t23_24;
 
 public class Main {
     public static void main(String[] args) {
-        MathObject worker = new MathObject();
+        MathObject mathObject = new MathObject();
         while (true) {
-            worker.getTasks();
-            worker.doTasks();
+            mathObject.readTasks();
+            mathObject.performUndoneMath();
 
             try {
                 Thread.sleep((long) ((Math.random()*1000)+1000));
-            } catch (InterruptedException e) {
-                e.printStackTrace();
+            } catch (InterruptedException err) {
+                err.printStackTrace();
             }
-            worker.getSavedJsonInfo();
+
         }
     }
 }
--- src/main/java/ru/mirea/t23_24/MathObject.java
+++ src/main/java/ru/mirea/t23_24/MathObject.java
@@ -15,103 +15,37 @@ import java.util.Collection;
 import java.util.List;
 
 public class MathObject {
-    Gson gson = new GsonBuilder().setPrettyPrinting().create();
     HttpClient httpClient = HttpClient.newHttpClient();
-    String taskURL = "http://80.87.199.76:3000/tasks";
-    String reportsURL = "http://80.87.199.76:3000/reports";
-    String myName = "Andrei Lyakhov";
-    List<JsonObject> tasks = new ArrayList<>();
-    List<Integer> taskIDs = new ArrayList<>();
+    Gson gson = new GsonBuilder().setPrettyPrinting().create();
 
-    String pathToDB = "src\\main\\java\\ru\\mirea\\t23_24\\db.json";
-    File file = new File(pathToDB);
-    FileWriter writer;
+    File file = new File("src\\main\\java\\ru\\mirea\\t23_24\\db.json");
+    FileWriter fileWriter;
     FileReader fileReader;
     JsonReader jsonReader;
 
+    String UserName = "Andrei Lyakhov";
+    String tasksAddress = "http://80.87.199.76:3000/tasks";
+    String resultAddress = "http://80.87.199.76:3000/reports";
+
+    List<Integer> tasksToWrite = new ArrayList<>();
+    List<JsonObject> tasksJson = new ArrayList<>();
+
+
+
+
     public MathObject() {
         try {
-            writer = new FileWriter(file, true);
+            fileWriter = new FileWriter(file, true);
             fileReader = new FileReader(file);
             jsonReader = new JsonReader(fileReader);
             if (fileReader.read() == -1) {
-                writer.write("[]");
-                writer.flush();
+                fileWriter.write("[]");
+                fileWriter.flush();
             }
-        } catch (IOException e) {
-            e.printStackTrace();
+        } catch (IOException err) {
+            err.printStackTrace();
         }
 
-    }
-
-    public void getTasks() {
-        tasks.clear();
-        HttpRequest request = HttpRequest.newBuilder().GET().uri(URI.create(taskURL)).build();
-        HttpResponse<String> response = null;
-        try {
-            response = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
-        } catch (InterruptedException | IOException e) {
-            e.printStackTrace();
-        }
-        JsonArray task = gson.fromJson(response.body(), JsonArray.class);
-        for (JsonElement je : task) {
-            tasks.add(je.getAsJsonObject());
-        }
-
-    }
-
-    public void doTasks() {
-
-        for (JsonObject task : tasks) {
-            if (!getSavedJsonInfo().contains(task.get("id").getAsInt())) {
-                System.out.println(task.get("taskDescription") + " calculated.");
-                double result = mathFunction(task.get("expression").getAsString());
-                System.out.println(result);
-          //      sendReport(task.get("id").getAsInt(), result);
-                taskIDs.add(task.get("id").getAsInt());
-            } else {
-                System.out.println("Skipped task "+task.get("id").getAsInt() + " as done");
-            }
-        }
-        try (FileWriter writer = new FileWriter(pathToDB)) {
-            writer.write(gson.toJson(taskIDs));
-            writer.flush();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    public double mathFunction(String exp) {
-        exp = exp.replace(" ", "");
-        String regular = "(?<=\\d)(?=\\D)|(?<=\\D)(?=\\D)|(?<=\\d\\D)(?=\\d)";
-        String[] split = exp.split(regular);
-        switch (split[1]) {
-            case "/":
-                return Math.round((double) Integer.parseInt(split[0]) / (double) Integer.parseInt(split[2])*100.0) / 100.0;
-            case "*":
-                return Integer.parseInt(split[0]) * Integer.parseInt(split[2]);
-            case "+":
-                return Integer.parseInt(split[0]) + Integer.parseInt(split[2]);
-            case "-":
-                return Integer.parseInt(split[0]) - Integer.parseInt(split[2]);
-
-        }
-        return -9999999;
-    }
-
-    public void sendReport(int taskId, double res) {
-
-        HttpRequest request = HttpRequest.newBuilder()
-                .POST(HttpRequest.BodyPublishers.ofString(gson.toJson(new Sender(0, taskId, myName, res))))
-                .uri(URI.create(reportsURL))
-                .setHeader("Content-Type", "application/json")
-                .build();
-        try {
-            HttpResponse<String> response = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
-            System.out.println(response.body());
-        } catch (IOException | InterruptedException e) {
-            e.printStackTrace();
-        }
     }
 
     public List<Integer> getSavedJsonInfo() {
@@ -132,5 +66,77 @@ public class MathObject {
         }
         return items;
     }
+
+    public void readTasks() {
+        tasksJson.clear();
+        HttpRequest request = HttpRequest.newBuilder().GET().uri(URI.create(tasksAddress)).build();
+        HttpResponse<String> response = null;
+        try {
+            response = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
+        } catch (InterruptedException | IOException e) {
+            e.printStackTrace();
+        }
+        JsonArray task = gson.fromJson(response.body(), JsonArray.class);
+        for (JsonElement je : task) {
+            tasksJson.add(je.getAsJsonObject());
+        }
+
+    }
+
+    public void performUndoneMath() {
+
+        for (JsonObject task : tasksJson) {
+            if (!getSavedJsonInfo().contains(task.get("id").getAsInt())) {
+                System.out.println(task.get("taskDescription") + " done");
+                double result = mathFunction(task.get("expression").getAsString());
+                System.out.println(result);
+                sendReport(task.get("id").getAsInt(), result);
+                tasksToWrite.add(task.get("id").getAsInt());
+            } else {
+                System.out.println("Skipped task "+task.get("id").getAsInt() + " as done");
+            }
+        }
+        try (FileWriter writer = new FileWriter("src\\main\\java\\ru\\mirea\\t23_24\\db.json")) {
+            writer.write(gson.toJson(tasksToWrite));
+            writer.flush();
+        } catch (IOException err) {
+            err.printStackTrace();
+        }
+    }
+
+    public double mathFunction(String taskInfo) {
+        taskInfo = taskInfo.replace(" ", "");
+        String splitReg = "(?<=\\d)(?=\\D)|(?<=\\D)(?=\\D)|(?<=\\d\\D)(?=\\d)";
+        String[] split = taskInfo.split(splitReg);
+        switch (split[1]) {
+            case "/":
+                return Math.round((double) Integer.parseInt(split[0]) / (double) Integer.parseInt(split[2])*100.0) / 100.0;
+            case "*":
+                return Integer.parseInt(split[0]) * Integer.parseInt(split[2]);
+            case "+":
+                return Integer.parseInt(split[0]) + Integer.parseInt(split[2]);
+            case "-":
+                return Integer.parseInt(split[0]) - Integer.parseInt(split[2]);
+
+        }
+        return -9999999;
+    }
+
+    public void sendReport(int taskId, double res) {
+
+        HttpRequest request = HttpRequest.newBuilder()
+                .POST(HttpRequest.BodyPublishers.ofString(gson.toJson(new Sender(0, taskId, UserName, res))))
+                .uri(URI.create(resultAddress))
+                .setHeader("Content-Type", "application/json")
+                .build();
+        try {
+            HttpResponse<String> response = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
+            System.out.println(response.body());
+        } catch (IOException | InterruptedException err) {
+            err.printStackTrace();
+        }
+    }
+
+
 }
 
